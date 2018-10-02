37359c400c2fe2bd1063c0b17528d9f8cf0c1949
raymondeyan <yanr3103@lgsstudent.org>
2018-10-02 07:41:29 -0700

    New

--- FracCalc.java
+++ FracCalc.java
@@ -6,44 +6,53 @@ public class FracCalc {
     {
         // TODO: Call produceAnswer with an equation
         //String input;
+
+        System.out.println("yoo");
         
         Scanner franner = new Scanner(System.in);
         String doo="";
         
         while(!(doo.equals("qwe")))
         {
-        System.out.println("\"qwe\" to quit");
-        System.out.println("");
-            System.out.println("Type your fraction:");
-        
-        doo = franner.nextLine();
-        String output = produceAnswer(doo);
-        System.out.println("Answer:");
-        System.out.println(output);
-        System.out.println("");
-    }
-    System.out.println("end");
+            System.out.println("\"qwe\" to quit");
+            System.out.println("");
+                System.out.println("Type your fraction:");
+            
+            doo = franner.nextLine();
+            String output = produceAnswer(doo);
+            System.out.println("Answer:");
+            System.out.println(output);
+            System.out.println("");
+        }
+        System.out.println("end");
     }
    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    /*public static int gcd2(int p, int q) {
-        while (q != 0) {
-            int temp = q;
-            q = p % q;
-            p = temp;
-        }
-        return p;
-    }
-    */
-    public static String produceAnswer(String doo)
-    { 
+        // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+        // This function takes a String 'input' and produces the result
+        //
+        // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+        //      e.g. input ==> "1/2 + 3/4"
+        //        n 
+        // The function should return the result of the fraction after it has been calculated
+        //      e.g. return ==> "1_1/4"
+       //public static int gcd2(int p, int q) {
+       //     while (q != 0) {
+       //         int temp = q;
+       //         q = p % q;
+       //         p = temp;
+       //     }
+       //     return p;
+       // }
+       
+       // source based on: https://stackoverflow.com/questions/6618994/simplifying-fractions-in-java
+       /** @return the greatest common denominator */
+       public static int gcm(int a, int b) 
+       {
+            return b == 0 ? a : gcm(b, a % b); 
+       }
+       
+       public static String produceAnswer(String doo)
+       { 
         // TODO: Implement this function to produce the solution to the input\
         int a;
         int b;
@@ -69,58 +78,86 @@ public class FracCalc {
         int whole2 = 0;
         int denom2 = 0;
         int num2 = 0;
-        boolean isfracnum1 = true;
-        boolean isfracnum2 = true;
-        boolean iswhole1 = true;
-        boolean iswhole2 = true;
+        boolean hasfracnum1 = true;
+        boolean hasfracnum2 = true;
+        boolean haswhole1 = true;
+        boolean haswhole2 = true;
         //boolean zero1 = false;
         //boolean zero2 = false;
         firstFrac = doo.substring(0, doo.indexOf(" "));
         //System.out.println("firstFrac: " + firstFrac);
         secondFrac = doo.substring(doo.indexOf(" ") +3);
-        //System.out.println("secondFrac: " + secondFrac);
+       // System.out.println("secondFrac: " + secondFrac);
         oper = doo.substring(doo.indexOf(" ") + 1,doo.indexOf(" ") + 2);
+        
+        boolean firstFracPositive = true;        
+        if (firstFrac.trim().startsWith("-"))
+        {
+            firstFracPositive = false;
+            firstFrac = firstFrac.substring(1);
+        }
+        
+        boolean secondFracPositive = true;        
+        if (secondFrac.trim().startsWith("-"))
+        {
+            secondFracPositive = false;
+            secondFrac = secondFrac.substring(1);
+        }
+        
         
         // frontslash not present
         if(firstFrac.indexOf("/")==-1)
         {
-            isfracnum1=false;
+            hasfracnum1=false;
         }
         
         if(secondFrac.indexOf("/")==-1)
         {
-            isfracnum2=false; 
+            hasfracnum2=false; 
         }
         
         if(firstFrac.indexOf("_")==-1)
         {
-            iswhole1=false;
+            haswhole1=false;
         }
         
         if(secondFrac.indexOf("_")==-1)
         {
-            iswhole2=false; 
+            haswhole2=false; 
         }
         
         //CONVERT 1stFRAC TO FRACTION
-        if(iswhole1 && isfracnum1)
+        if(haswhole1 && hasfracnum1)
         // first frac: x_x/xx
         {
-           whole1=Integer.parseInt(firstFrac.substring(0,firstFrac.indexOf("_")));
-           //System.out.println("whole1: " + firstFrac.substring(0,firstFrac.indexOf("_")));
-           //System.out.println("denom1: " + firstFrac.substring(firstFrac.indexOf("/")+1));
-           denom1=Integer.parseInt(firstFrac.substring(firstFrac.indexOf("/")+1));
-           num1=Integer.parseInt(firstFrac.substring(firstFrac.indexOf("_")+1,firstFrac.indexOf("/")));
+              num1=Integer.parseInt(firstFrac.substring(firstFrac.indexOf("_")+1,firstFrac.indexOf("/"))); 
+          
+              whole1=Integer.parseInt(firstFrac.substring(0,firstFrac.indexOf("_")));
+               //System.out.println("whole1: " + firstFrac.substring(0,firstFrac.indexOf("_")));
+               //System.out.println("denom1: " + firstFrac.substring(firstFrac.indexOf("/")+1));
+               denom1=Integer.parseInt(firstFrac.substring(firstFrac.indexOf("/")+1));
+               
+//                System.out.println("condition if(haswhole1 && hasfracnum1)");
+//                System.out.println("num1: " + num1);
+//                System.out.println("whole1: " + whole1);
+//                System.out.println("denom1: " + denom1);
         }
         
-        if(!iswhole1&&isfracnum1)
+        
+        
+        
+        if(!haswhole1 && hasfracnum1)
         // first frac: x/xx
         {
            denom1=Integer.parseInt(firstFrac.substring(firstFrac.indexOf("/")+1));
            num1=Integer.parseInt(firstFrac.substring(0,firstFrac.indexOf("/")));
+           
+//            System.out.println("if(!haswhole1&&hasfracnum1)");
+//            System.out.println("num1: " + num1);
+//            System.out.println("denom1: " + denom1);
         }
         
-        if(!iswhole1&&!isfracnum1)
+        if(!haswhole1 && !hasfracnum1)
         // first frac: xx
         {
            denom1=1;
@@ -128,22 +165,24 @@ public class FracCalc {
         }
         
         //CONVERT 2ndFRAC TO FRACTION
-        if(iswhole2&&isfracnum2)
+        if(haswhole2 && hasfracnum2)
         // second frac: x_x/xx
         {
-           whole2=Integer.parseInt(secondFrac.substring(0,secondFrac.indexOf("_")));
+              num2=Integer.parseInt(secondFrac.substring(secondFrac.indexOf("_")+1,secondFrac.indexOf("/")));
+             
+              whole2=Integer.parseInt(secondFrac.substring(0,secondFrac.indexOf("_")));
+           //whole2=Integer.parseInt(secondFrac.substring(0,secondFrac.indexOf("_")));
            denom2=Integer.parseInt(secondFrac.substring(secondFrac.indexOf("/")+1));
-           num2=Integer.parseInt(secondFrac.substring(secondFrac.indexOf("_")+1,secondFrac.indexOf("/")));
         }
         
-        if(!iswhole2&&isfracnum2)
+        if(!haswhole2 && hasfracnum2)
         // second frac: x/xx
         {
            denom2=Integer.parseInt(secondFrac.substring(secondFrac.indexOf("/")+1));
            num2=Integer.parseInt(secondFrac.substring(0,secondFrac.indexOf("/")));
         }
         
-        if(!iswhole2&&!isfracnum2)
+        if(!haswhole2 && !hasfracnum2)
         // second frac: xx
         {
            denom2=1;
@@ -170,69 +209,86 @@ public class FracCalc {
         //String checkpoint2 = "whole:"+ whole2 + " " + "numerator:" + num2 + " " + "denominator:" + denom2;
         //return checkpoint2;
         //return "something";
+        //negative check:
+        
         //reduction:
-        if(iswhole1 && isfracnum1)
+        if(haswhole1 && hasfracnum1)
         // first frac: x_x/xx
         {
-           num1=(whole1*denom1)+num1;
+           num1 = (whole1 * denom1) + num1;
            //System.out.println(num1);
         }
         
-        if(!iswhole1&&!isfracnum1)
+        if(!haswhole1 && !hasfracnum1)
         {
             denom1=1;
            num1=Integer.parseInt(firstFrac);
         }
 
-        if(!iswhole1&&isfracnum1)
+        if(!haswhole1 && hasfracnum1)
         {
             num1=num1;
             denom1= denom1;
         }
         
-        if(iswhole2&&isfracnum2)
+        if(haswhole2 && hasfracnum2)
         // second frac: x_x/xx
         {
-           num2=(whole2*denom2)+num1;
+           num2 = (whole2 * denom2) + num2;
            //System.out.println(num2);
         }
         
-        if(!iswhole2&&!isfracnum2)
+        if(!haswhole2 && !hasfracnum2)
         {
             denom2=1;
            num2=Integer.parseInt(secondFrac);
         }
         
-        if(!iswhole2&&isfracnum2)
+        if(!haswhole2 && hasfracnum2)
         {
             num2=num2;
             denom2= denom2;
         }
-        /* System.out.println(num1);
-         System.out.println(denom1);
-         System.out.println(num2);
-         System.out.println(denom2);
-         */
+//          System.out.println("num1: " + num1);
+//          System.out.println("denom1: " + denom1);
+//          System.out.println("num2: " + num2);
+//          System.out.println("denom2: " + denom2);
          
+         if (!firstFracPositive)
+        {
+            num1 = -num1;
+        }
+        
+        if (!secondFracPositive)
+        {
+            num2 = -num2;
+        }    
          
-        int newNum1=num1*denom2; 
+        int newNum1 = num1 * denom2; 
         
-        int newDenom1=denom1*denom2;
+        int newDenom1 = denom1 * denom2;
         
-        int newNum2=num2*denom1;
+        int newNum2 = num2 * denom1;
         //denom was changed last line
-        int newDenom2=denom2*denom1;
+        int newDenom2 = denom2 * denom1;
         //debugger
         //System.out.println(num1 + "    " + num2 + "    " + denom1 + "    " + denom2 + "    " );
         int finalFracNum = 0;
         int finalFracDenom = 0;
         int finalWhole = 0;
+        
+        //System.out.println("oper: " + oper);
+        
+        
         if(oper.equals("*"))
         {
-            finalFracNum=newNum1*newNum2; 
-            finalFracDenom=newDenom1*newDenom2; 
+            //finalFracNum = newNum1 * newNum2; 
+            //finalFracDenom = newDenom1 * newDenom2; 
             
-            if(finalFracDenom==0||finalFracNum==0) //special case
+            finalFracNum = num1 * num2; 
+            finalFracDenom = denom1 * denom2;
+            
+            if(finalFracDenom==0 || finalFracNum==0) //special case
             {
                 return "0";
             }
@@ -240,15 +296,21 @@ public class FracCalc {
         //System.out.println(denom1);
         if(oper.equals("+"))
         {
-            finalFracNum=num1+num2; 
-            finalFracDenom=denom1; 
+           // finalFracNum=num1+num2; 
+           // finalFracDenom=denom1; 
+            finalFracNum = newNum1 + newNum2; 
+            finalFracDenom = newDenom1; 
         }
         
         if(oper.equals("/"))
         {
-            finalFracNum=newNum1/newNum2; 
-            finalFracDenom=newDenom1/newDenom2;
-            if((finalFracDenom)==0) //special case
+            //finalFracNum = newNum1/newNum2; 
+            //finalFracDenom=newDenom1/newDenom2;
+            
+            finalFracNum = num1 * denom2; 
+            finalFracDenom = num2 * denom1;
+            
+            if((finalFracDenom) == 0) //special case
             {
                 return "error";
             }
@@ -261,45 +323,99 @@ public class FracCalc {
         
         if(oper.equals("-"))
         {
-            finalFracNum=num1-num2; 
-            finalFracDenom=denom1; 
+            //finalFracNum = num1-num2; 
+            //finalFracDenom = denom1; 
+            
+            finalFracNum = newNum1 - newNum2; 
+            finalFracDenom = newDenom1;
         }
         
+      //  System.out.println("finalFracNum: " + finalFracNum);
+      //  System.out.println("finalFracDenom: " + finalFracDenom);
         
-            //System.out.println(finalFracNum);
-            //System.out.println(finalFracDenom);
-        //simplify:
-        
-        while((finalFracNum%2==0&&finalFracDenom%2==0)||(finalFracNum%3==0&&finalFracDenom%3==0))
+        boolean finalPositive = true;
+        if (finalFracNum * finalFracDenom < 0)
         {
-          if(finalFracNum%2==0&&finalFracDenom%2==0)
+            finalPositive = false;
+        }
+        
+        finalFracNum = Math.abs(finalFracNum);
+        finalFracDenom = Math.abs(finalFracDenom);
+        
+        //simplify:
+        //int absFinalFracNum = Math.abs(finalFracNum);
+        /*
+        while((finalFracNum%2 == 0 && finalFracDenom%2 == 0) || (finalFracNum%3 == 0 && finalFracDenom%3 == 0))
+        {
+          if(finalFracNum%2 == 0 && finalFracDenom%2 == 0)
           {
               finalFracNum=finalFracNum/2;
               finalFracDenom=finalFracDenom/2;
           }
           
-          if(finalFracNum%3==0&&finalFracDenom%3==0)
+          if(finalFracNum%3 == 0 && finalFracDenom%3 == 0)
           {
              finalFracNum=finalFracNum/3;
              finalFracDenom=finalFracDenom/3;
           }
-        }
+        }*/
+      
+       int gcm = gcm(finalFracNum, finalFracDenom);
+       System.out.println("gcm: " + gcm);
+       
+       finalFracNum = finalFracNum / gcm;
+       finalFracDenom = finalFracDenom / gcm; 
+      
+      //  System.out.println("finalFracNum: " + finalFracNum);
+      //  System.out.println("finalFracDenom: " + finalFracDenom);
         
-        while(finalFracNum>=finalFracDenom)
+        
+       
+        while (finalFracNum >= finalFracDenom)
         {
-             finalFracNum-=finalFracDenom;
+            //System.out.println("finalFracNum: " + finalFracNum);
+            //System.out.println("finalFracDenom: " + finalFracDenom);
+            //System.out.println("finalWhole: " + finalWhole);
+             //absFinalFracNum-=absFinalFracNum;
+             finalFracNum -= finalFracDenom;
              finalWhole++;
         }
         
-        if(finalWhole==0)
+       /* if (finalFracNum >= finalFracDenom)
         {
-            return finalFracNum + "/" + finalFracDenom;
+            finalWhole = finalFracNum / finalFracDenom;
+            finalFracNum = finalFracNum % finalFracDenom;
+        }*/
+        
+        
+        //System.out.println("finalFracDenom: " + finalFracDenom);
+       // System.out.println("finalWhole: " + finalWhole);
+        
+        String result;
+        
+        if(finalFracNum == 0)
+        {
+            System.out.println("finalFracNum == 0");
+            result = "" +  finalWhole;
+        }  
+        else if(finalWhole==0)
+        {
+          //  System.out.println("finalWhole==0");
+            result = finalFracNum + "/" + finalFracDenom;
         }
         else
         {
-            return finalWhole + "_" + finalFracNum + "/" + finalFracDenom;
+          //  System.out.println("else condition");
+            result = finalWhole + "_" + finalFracNum + "/" + finalFracDenom;
         }
+        
+        if (!finalPositive) {
+            result = "-" + result;
+        }
+        
+      //  System.out.println("result: " + result);
+        return result;
         //return "yeet";
     // TODO: Fill in the space below with any helper methods that you think you will need
-}
+    }
 }
